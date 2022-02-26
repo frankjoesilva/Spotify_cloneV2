fe6a5b7cd9a87aab2d5f659bfaa2183d1266f2ed
Frank Silva <frankjoesilva@gmail.com>
2022-02-25 16:46:39 -0800

    login up and running

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login'
+import { getTokenFromUrl } from './spotify'
+
 
 export default function App() {
+  const [token, setToken] = useState()
+
+  useEffect(() => {
+    const token = getTokenFromUrl()
+    window.location.hash = ""
+    console.log("token", token);
+  }, []);
+
   return (
     <div className="app">
       <Login />
--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Login.css'
+import { loginUrl } from './spotify'
 
 export default function Login() {
     return (
@@ -8,7 +9,7 @@ export default function Login() {
                 src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
                 alt=""
             />
-            <button href=''>LOGIN WITH SPOTIFY</button>
+            <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
         </div>
     )
 }
