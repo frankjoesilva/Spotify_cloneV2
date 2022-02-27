52fec3b37c532b4992cdcddcfdf765be1308d83a
Frank Silva <frankjoesilva@gmail.com>
2022-02-27 09:52:34 -0800

    continuing reducer

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Login from './Login';
 import Player from './Player';
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from 'spotify-web-api-js';
+import { useDataLayerValue } from './DataLayer'
 
 const spotify = new SpotifyWebApi();
 
 export default function App() {
-  const [token, setToken] = useState(null)
+  // const [token, setToken] = useState(null)
+  const [{ user, token }, dispatch] = useDataLayerValue()
 
 
   useEffect(() => {
@@ -17,16 +19,23 @@ export default function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token)
+      dispatch({
+        type: 'SET_TOKEN',
+        token: _token
+      })
 
       spotify.setAccessToken(_token)
       spotify.getMe().then(user => {
-        console.log('user', user)
+
+
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        })
       })
     }
-
-    console.log("token", token);
   }, []);
+
 
   return (
     <div className="app">{token ?
--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,8 @@ export const initialState = {
     playlists: [],
     playing: false,
     item: null,
+    token: 'BQB95IcrJJyt-DejNBc8KNQ64KieBpdJOv8xD2tnrOr-a9M2ljd9A5vj25U5RtZJnScMde_PDwE7joNn-oGIsc_xa7kI7EsbkSyYLG0n5z_R1C9U5HD70jWITxdD54RmlF0JWbHWbDg3r_0x8TQM59EDSF7y6UgQ-ExdOh7dzg'
+    ,
 };
 
 const reducer = (state, action) => {
@@ -13,6 +15,11 @@ const reducer = (state, action) => {
                 ...state,
                 user: action.user,
             };
+        case "SET_TOKEN":
+            return {
+                ...state,
+                token: action.token
+            }
         default:
             return state
     }
