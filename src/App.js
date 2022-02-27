846417e3949db7611e85c79dabb02b5bb5649dc5
Frank Silva <frankjoesilva@gmail.com>
2022-02-26 19:27:55 -0800

    created data layer and starter creating reducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import Login from './Login'
-import { getTokenFromUrl } from './spotify'
+import Login from './Login';
+import Player from './Player';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
 
+const spotify = new SpotifyWebApi();
 
 export default function App() {
   const [token, setToken] = useState(null)
@@ -15,13 +18,20 @@ export default function App() {
 
     if (_token) {
       setToken(_token)
+
+      spotify.setAccessToken(_token)
+      spotify.getMe().then(user => {
+        console.log('user', user)
+      })
     }
 
     console.log("token", token);
   }, []);
 
   return (
-    <div className="app">{token ? (<h1>logged in</h1>) : (<Login />)} </div>
+    <div className="app">{token ?
+      <Player />
+      : <Login />} </div>
   )
 }
 
