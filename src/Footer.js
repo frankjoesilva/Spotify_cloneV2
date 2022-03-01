9d7a1cae204f3357cf1363e78d843a2c5375c50a
Frank Silva <frankjoesilva@gmail.com>
2022-03-01 09:42:34 -0800

    added player icons in footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import './Footer.css'
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
 
 export default function Footer() {
     return (
@@ -9,7 +14,11 @@ export default function Footer() {
             </div>
 
             <div className="footer_center">
-                <h1>player controls</h1>
+                <ShuffleIcon className='footer_green' />
+                <SkipPreviousIcon className='footer_icon' />
+                <PlayCircleOutlineIcon fontSize='large' className='footer_icon' />
+                <SkipNextIcon className='footer_icon' />
+                <RepeatIcon className='footer_icon' />
             </div>
 
             <div className="footer_right">
