243cf61a344933fe2b0c13d1f618702a8bd465b7
Frank Silva <frankjoesilva@gmail.com>
2022-03-01 09:29:54 -0800

    started work on player in footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,17 @@ import './Footer.css'
 export default function Footer() {
     return (
         <div className="footer">
-            Footer
+            <div className="footer_left">
+                <h1>Album and song details</h1>
+            </div>
+
+            <div className="footer_center">
+                <h1>player controls</h1>
+            </div>
+
+            <div className="footer_right">
+                <p>volume</p>
+            </div>
         </div>
     )
 }
